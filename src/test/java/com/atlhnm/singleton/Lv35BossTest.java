563d8c04de7d2e533b5c05e6cc8e185d6ad24794
Dima Krasilov <dfkrasilov@gmail.com>
2019-01-15 08:45:18 +0300

    testing singleton

--- src/test/java/com/atlhnm/singleton/Lv35BossTest.java
+++ src/test/java/com/atlhnm/singleton/Lv35BossTest.java
@@ -1,4 +1,13 @@
 package com.atlhnm.singleton;
 
-public class Lv35BossTest {
+public class Lv35BossTest implements SimpleSingletonTest, ConcurrentSingletonTest {
+    @Override
+    public Class<?> singletonClass() {
+        return Lv35Boss.class;
+    }
+
+    @Override
+    public Object getInstance(Class<?> aClass) {
+        return Lv35Boss.INSTANCE;
+    }
 }
